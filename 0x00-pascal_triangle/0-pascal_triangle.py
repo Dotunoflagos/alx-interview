b24d0bb3ce2d00878373172760bd568685f3e754
dotunoflagos <adedotun_balogun@yahoo.com>
2023-04-18 13:39:34 +0100

    0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import math
+"""
+Pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """Pascal triangle"""
+    triangle = []
+
+    def get_row(n):
+        """Get nth term of row"""
+        row = []
+        for i in range(n+1):
+            result = math.comb(n, i)
+            row.append(result)
+        return row
+
+    for i in range(n):
+        triangle.append(get_row(i))
+    return triangle
