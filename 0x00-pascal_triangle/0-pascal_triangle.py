82b578ae03b01bcb39032665a2f2fef364e5d7b3
dotunoflagos <adedotun_balogun@yahoo.com>
2023-04-18 13:56:57 +0100

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,12 +7,25 @@ Pascal_triangle
 def pascal_triangle(n):
     """Pascal triangle"""
     triangle = []
+    def coefficient(n, k):
+        if k > n:
+            return 0
+        if k == 0 or k == n:
+            return 1
+
+        numerator = 1
+        denominator = 1
+        for i in range(k):
+            numerator *= n - i
+            denominator *= i + 1
+
+        return numerator // denominator
 
     def get_row(n):
         """Get nth term of row"""
         row = []
         for i in range(n+1):
-            result = math.comb(n, i)
+            result = coefficient(n, i)
             row.append(result)
         return row
 
