2aacdde336f4e183f9a4f78306dd0d73f4a645d8
BAlogun Adedotun <Adedotun_balogun@yahoo.com>
2023-08-16 17:54:13 +0100

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""a
+"""
+
+
+def canUnlockAll(boxes):
+    """Determines if boxes can be unlocked"""
+    position = 0
+    unlocked = {}
+
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
+    return False
